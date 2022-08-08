3cb42bbd4b24e1dbc28d8d44f6e2b343b7edc597
StergiosKo <stergiosk202@gmail.com>
2022-08-08 20:03:22 +0300

    Finishing html file and starting with filter button

--- static-job-listings-master/script.js
+++ static-job-listings-master/script.js
@@ -2,31 +2,22 @@ const button = document.querySelector("#testButton");
 const jobArray = Array.from(document.querySelectorAll('.job-listing'));
 
 button.addEventListener('click', function(){
-    let filterData = Array.from(document.querySelectorAll('#filter>div>ul'));
+
     jobArray.forEach(element => {
-        if (hasAllFilterData(element, filterData)){
-            element.style.display = 'none';
-        }
-        else element.style.display = 'block';
+        element.style.display = 'none';
     })
+
+    let filterData = Array.from(document.querySelectorAll('#filter>div>ul>li'));
+    // filterData.forEach(aData => {
+    //     console.log(aData.dataset);
+    //     let dataFinds = document.querySelectorAll(aData.dataset);
+    //     dataFinds.forEach(element => {
+    //         element.parentElement.parentElement.display = 'block'
+    //     });
+    // });
+    let dataFinds = Array.from(document.querySelectorAll('.job-listing ul [data-role-type="frontend"]'));
+    dataFinds.forEach(element => {
+        console.log(element.parentElement.parentElement)
+        element.parentElement.parentElement.style.display = 'block';
+    });
 })
-
-function hasAllFilterData(jobElement, filterData){
-    let listItemsData = jobElement.querySelectorAll('ul>li');
-    filterData.forEach(element => {
-        if(!hasSpecificFilterData(listItemsData, element)){
-            return false;
-        }
-    });
-    return true;
-}
-
-function hasSpecificFilterData(elements, data){
-    elements.forEach(element => {
-        console.log(element);
-        if (element.hasAttribute(data)){
-            return true;
-        }
-    });
-    return false;
-}
