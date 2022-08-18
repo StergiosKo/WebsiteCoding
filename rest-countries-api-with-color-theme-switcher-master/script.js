b1db9d6e31ed27b3a9c93d9bd66303b65f62f8a0
StergiosKo <stergiosk202@gmail.com>
2022-08-18 19:04:40 +0300

    Creating countries with Template Literals and Detail page optimized

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,8 +1,3 @@
-const countriesDataUrl = "https://restcountries.com/v3.1/all";
-const greeceUrl = "https://restcountries.com/v3.1/alpha/gr";
-
-const countriesContainer = document.querySelector('#countries-container');
-
 const CountriesApi = function(selector){
     const countriesDataUrl = "https://restcountries.com/v3.1/all";
     const countriesContainer = document.querySelector(selector);
@@ -21,98 +16,36 @@ const CountriesApi = function(selector){
 
     async function createCountry(countryData) {
 
-        // let html = `<div class='country'>
-        //     <img src=${countryData.flags.svg}>
-        //     <div>
-        //         <h1>${countryData.name.common}</h1>
-        //         <ul>
-        //         <li><h2>Population</h2><p>${countryData.population}</p></li>
-        //         <li class='region'><h2>Region</h2><p>${countryData.region}</p></li>
-        //         <li><h2>Capital</h2><p>${countryData.capital}</p></li>
-        //         </ul>
-        //     </div>
-        // </div>`;
-        // countriesContainer.insertAdjacentHTML("beforeend", html);
-    
-        // Parent
-        let parentDiv = document.createElement('div');
-        parentDiv.classList.add('country');
-        parentDiv.addEventListener('click', function(){
-            // sessionStorage.setItem('countryName' ,countryData.cca2);
-            // location.href = 'details.html?country=' + countryData.cca2;
+        let html = `<div class='country' data-cca2=${countryData.cca2}>
+            <img src=${countryData.flags.svg}>
+            <div>
+                <h1>${countryData.name.common}</h1>
+                <ul>
+                <li><h2>Population</h2><p>${countryData.population}</p></li>
+                <li class='region'><h2>Region</h2><p>${countryData.region}</p></li>
+                <li><h2>Capital</h2><p>${countryData.capital}</p></li>
+                </ul>
+            </div>
+        </div>`;
+        countriesContainer.insertAdjacentHTML("beforeend", html);
+        document.querySelector(`[data-cca2=${countryData.cca2}]`).addEventListener('click', function(){
             var para = new URLSearchParams();
             para.append("country", countryData.cca2);
             location.href = "details.html?" + para.toString();
         })
-        countriesContainer.appendChild(parentDiv);
-    
-        // Should put parameters in url instead of sessionstorage
-    
-        // Image
-        let flagImg = document.createElement('img');
-        flagImg.src = countryData.flags.svg;
-        parentDiv.appendChild(flagImg);
-    
-        // Text Container
-        let textContainerDiv = document.createElement('div')
-        parentDiv.append(textContainerDiv);
-    
-        // Country Name
-        let nameHeader = document.createElement('h1');
-        nameHeader.textContent = countryData.name.common;
-        textContainerDiv.appendChild(nameHeader);
-    
-        // Ul
-        let ulist = document.createElement('ul');
-        textContainerDiv.appendChild(ulist);
-    
-        // Population
-        let populLi = document.createElement('li');
-        let populHeader = document.createElement('h2');
-        populHeader.textContent = 'Population:';
-        populLi.appendChild(populHeader);
-    
-        let populNumber = document.createElement('p');
-        populNumber.textContent = countryData.population.toLocaleString('en-UK').replaceAll(',','.');
-        populLi.appendChild(populNumber);
-        ulist.appendChild(populLi);
-    
-        // Region
-        let regionLi = document.createElement('li');
-        regionLi.classList.add('region')
-        let regionHeader = document.createElement('h2');
-        regionHeader.textContent = 'Region:';
-        regionLi.appendChild(regionHeader);
-    
-        let regionName = document.createElement('p');
-        regionName.textContent = countryData.region;
-        regionLi.appendChild(regionName);
-        ulist.appendChild(regionLi);
-    
-        // Capital
-        let capitalLi = document.createElement('li');
-        let capitalHeader = document.createElement('h2');
-        capitalHeader.textContent = 'Capital:';
-        capitalLi.appendChild(capitalHeader);
-    
-        let capitalName = document.createElement('p');
-        capitalName.textContent = countryData.capital;
-        capitalLi.appendChild(capitalName);
-        ulist.appendChild(capitalLi);
-      
-    }
+     }
 
     function toggleLightDarkMode(){
         var body = document.querySelector('body');
         if (body.getAttribute('data-theme') == 'default'){
             body.removeAttribute('data-theme');
             body.setAttribute('data-theme', 'light');
-            sessionStorage.setItem('mode', 'light');
+            localStorage.setItem('mode', 'light');
         }
         else{
             body.removeAttribute('data-theme');
             body.setAttribute('data-theme', 'default');
-            sessionStorage.setItem('mode', 'dark');
+            localStorage.setItem('mode', 'dark');
         }
     }
     
@@ -211,10 +144,6 @@ const CountriesApi = function(selector){
         })
     })
 
-    if (sessionStorage.getItem('mode') == 'light'){
-        toggleLightDarkMode();
-    }
-
     const init = function () {
         
         createAllCountries();
@@ -236,227 +165,17 @@ const CountriesApi = function(selector){
         lightModeButton.addEventListener('click', (evt) => toggleLightDarkMode())
 
         // Should change with local storage
-        if (sessionStorage.getItem('mode') == 'light'){
+        if (localStorage.getItem('mode') == 'light'){
             toggleLightDarkMode();
         }
     }
 
     init();
 
-    return {
-        filterByRegion
-    }
+    // If another object required access to methods
+    // return {
+    //     filterByRegion
+    // }
 }
 
 let ccapi = new CountriesApi('#countries-container');
-
-// async function createAllCountries(){
-//     const res = await fetch(countriesDataUrl);
-
-//     let countriesData = await res.json();
-//     countriesData.forEach(country => {
-//         createCountry(country);
-//     });
-// }
-
-// createAllCountries();
-
-// async function createCountry(countryData) {
-
-//     // let html = `<div class='country'>
-//     //     <img src=${countryData.flags.svg}>
-//     //     <div>
-//     //         <h1>${countryData.name.common}</h1>
-//     //         <ul>
-//     //         <li><h2>Population</h2><p>${countryData.population}</p></li>
-//     //         <li class='region'><h2>Region</h2><p>${countryData.region}</p></li>
-//     //         <li><h2>Capital</h2><p>${countryData.capital}</p></li>
-//     //         </ul>
-//     //     </div>
-//     // </div>`;
-//     // countriesContainer.insertAdjacentHTML("beforeend", html);
-
-//     // Parent
-//     let parentDiv = document.createElement('div');
-//     parentDiv.classList.add('country');
-//     parentDiv.addEventListener('click', function(){
-//         // sessionStorage.setItem('countryName' ,countryData.cca2);
-//         // location.href = 'details.html?country=' + countryData.cca2;
-//         var para = new URLSearchParams();
-//         para.append("country", countryData.cca2);
-//         location.href = "details.html?" + para.toString();
-//     })
-//     countriesContainer.appendChild(parentDiv);
-
-//     // Should put parameters in url instead of sessionstorage
-
-//     // Image
-//     let flagImg = document.createElement('img');
-//     flagImg.src = countryData.flags.svg;
-//     parentDiv.appendChild(flagImg);
-
-//     // Text Container
-//     let textContainerDiv = document.createElement('div')
-//     parentDiv.append(textContainerDiv);
-
-//     // Country Name
-//     let nameHeader = document.createElement('h1');
-//     nameHeader.textContent = countryData.name.common;
-//     textContainerDiv.appendChild(nameHeader);
-
-//     // Ul
-//     let ulist = document.createElement('ul');
-//     textContainerDiv.appendChild(ulist);
-
-//     // Population
-//     let populLi = document.createElement('li');
-//     let populHeader = document.createElement('h2');
-//     populHeader.textContent = 'Population:';
-//     populLi.appendChild(populHeader);
-
-//     let populNumber = document.createElement('p');
-//     populNumber.textContent = countryData.population.toLocaleString('en-UK').replaceAll(',','.');
-//     populLi.appendChild(populNumber);
-//     ulist.appendChild(populLi);
-
-//     // Region
-//     let regionLi = document.createElement('li');
-//     regionLi.classList.add('region')
-//     let regionHeader = document.createElement('h2');
-//     regionHeader.textContent = 'Region:';
-//     regionLi.appendChild(regionHeader);
-
-//     let regionName = document.createElement('p');
-//     regionName.textContent = countryData.region;
-//     regionLi.appendChild(regionName);
-//     ulist.appendChild(regionLi);
-
-//     // Capital
-//     let capitalLi = document.createElement('li');
-//     let capitalHeader = document.createElement('h2');
-//     capitalHeader.textContent = 'Capital:';
-//     capitalLi.appendChild(capitalHeader);
-
-//     let capitalName = document.createElement('p');
-//     capitalName.textContent = countryData.capital;
-//     capitalLi.appendChild(capitalName);
-//     ulist.appendChild(capitalLi);
-  
-// }
-
-
-
-// function toggleLightDarkMode(){
-//     var body = document.querySelector('body');
-//     if (body.getAttribute('data-theme') == 'default'){
-//         body.removeAttribute('data-theme');
-//         body.setAttribute('data-theme', 'light');
-//         sessionStorage.setItem('mode', 'light');
-//     }
-//     else{
-//         body.removeAttribute('data-theme');
-//         body.setAttribute('data-theme', 'default');
-//         sessionStorage.setItem('mode', 'dark');
-//     }
-// }
-
-// function filterByRegion(region){
-//     countriesContainer.innerHTML = '';
-//     let searchInput = document.querySelector('#searchInput');
-
-//     if (region !== 'All'){
-
-        
-//         if (!searchInput.value){
-//             fetch('https://restcountries.com/v3.1/region/' + region.toLowerCase())
-//             .then(res =>{return res.json()})
-//             .then(data=>{
-//                 data.forEach(country => {
-//                     createCountry(country);
-//                 })
-//             })
-//             .catch(error=>console.log(error))
-//         }
-//         else{
-//             searchFunction();
-//         }
-
-//     }
-//     else{
-//         if (!searchInput.value){
-//             createAllCountries()
-//         }
-//         else{
-//             searchFunction();
-//         }
-//     }
-
-// }
-
-
-// function searchFunction(){
-
-//     //Delaying the function execute
-//     if (this.timer) {
-//         window.clearTimeout(this.timer);
-//     }
-//     this.timer = window.setTimeout(function() {
-    
-//         //Execute the function code here...
-//         countriesContainer.innerHTML = '';
-//         // removeActiveItem()
-//         let regionFilter = document.querySelector('.active');
-//         if (regionFilter !== null) {regionFilter = regionFilter.textContent}
-//         let inputText = document.querySelector('#searchInput').value.toLowerCase();
-
-//         if(inputText != ''){
-        
-//             fetch('https://restcountries.com/v3.1/name/' + inputText)
-//             .then(res =>{return res.json()})
-//             .then(data=>{
-//                 data.forEach(country => {
-//                     if (regionFilter != 'All' && regionFilter != null){
-//                         if (regionFilter == country.region){
-//                             createCountry(country);
-//                         }
-//                     }
-//                     else{
-//                         createCountry(country);
-//                     }
-                    
-//                 })
-//             })
-//             .catch(error=>console.log(error))
-//         }
-//         else{
-//             createAllCountries()
-//         }
-
-    
-//     }, 500);
-// }
-
-
-// function removeActiveItem(){
-//     let getElemWithClass = document.querySelector('.active');
-//     if (getElemWithClass !== null) {
-//       getElemWithClass.classList.remove('active');
-//     }
-// }
-
-// //get all the elements list-group-item
-// document.querySelectorAll('ul.dropdown-content > li').forEach(function(item) {
-//     // iterate and add event lstener to each of them
-//     item.addEventListener('click', function(elem) {
-//       // check if any element have a class active
-//       // if so then remove the class from it
-//       removeActiveItem()
-//       //add the active class to the element from which click event triggered
-//       item.classList.add('active')
-  
-//     })
-// })
-
-// if (sessionStorage.getItem('mode') == 'light'){
-//     toggleLightDarkMode();
-// }
--- rest-countries-api-with-color-theme-switcher-master/scriptDetails.js
+++ rest-countries-api-with-color-theme-switcher-master/scriptDetails.js
@@ -1,103 +1,111 @@
-function toggleLightDarkMode(){
-    var body = document.querySelector('body');
-    if (body.getAttribute('data-theme') == 'default'){
-        body.removeAttribute('data-theme');
-        body.setAttribute('data-theme', 'light');
-        sessionStorage.setItem('mode', 'light');
+const countryDetailsApi = function(){
+
+    const lightModeButton = document.querySelector('#lightModeButton');
+    const backButton = document.querySelector('.back-button');
+    const homeButton = document.querySelector('#homeButton');
+
+    function toggleLightDarkMode(){
+        var body = document.querySelector('body');
+        if (body.getAttribute('data-theme') == 'default'){
+            body.removeAttribute('data-theme');
+            body.setAttribute('data-theme', 'light');
+            localStorage.setItem('mode', 'light');
+        }
+        else{
+            body.removeAttribute('data-theme');
+            body.setAttribute('data-theme', 'default');
+            localStorage.setItem('mode', 'dark');
+        }
     }
-    else{
-        body.removeAttribute('data-theme');
-        body.setAttribute('data-theme', 'default');
-        sessionStorage.setItem('mode', 'dark');
+
+    async function displayCountry() {
+        let params = new URLSearchParams(window.location.search)
+        let countryUrl = params.get('country')
+    
+        const res = await fetch("https://restcountries.com/v3.1/alpha/" + countryUrl)
+      
+        let countryData = await res.json();
+      
+        countryData = countryData[0];
+    
+    
+        // console.log(countryUrl);
+        
+        document.querySelector('#country-flag').src = countryData.flags.svg
+        
+        document.querySelector("#official-name").textContent = countryData.name.common;
+        
+        document.querySelector("#native-name").textContent = Object.values(countryData.name.nativeName)[0].common;
+        
+        document.querySelector("#population").textContent = countryData.population.toLocaleString('en-UK').replaceAll(',','.');
+        
+        document.querySelector("#region").textContent = countryData.region;
+    
+        document.querySelector("#sub-region").textContent = countryData.subregion;
+        
+        document.querySelector("#capital").textContent = countryData.capital;
+        
+        document.querySelector("#top-level-domain").textContent = countryData.tld;
+        
+        // Currencies
+        let currencies = [];
+        for(var el in countryData.currencies){
+            currencies.push(countryData.currencies[el].name);
+        }
+        let currenciesString = currencies.join(', ');
+        document.querySelector("#currencies").textContent = currenciesString;
+    
+        // Languages
+        let languages = [];
+        for(var el in countryData.languages){
+            languages.push(countryData.languages[el]);
+        }
+        let languagesString = languages.join(', ');
+        document.querySelector("#languages").textContent = languagesString;
+    
+        // Borders
+        bordersContainer = document.querySelector('#borders-container');
+        if (countryData.borders !== undefined){
+            countryData.borders.forEach(element => {
+    
+                let countryListItem = document.createElement('li');
+                countryListItem.classList.add('details-button');
+                bordersContainer.appendChild(countryListItem);
+        
+                fetch("https://restcountries.com/v3.1/alpha/" + element)
+                .then((response) => response.json())
+                .then((data) => countryListItem.textContent = data[0].name.common)
+                .then((data) => countryListItem.addEventListener('click', function(){
+                    sessionStorage.setItem('countryName' ,data.cca2);
+                    var url = new URL(window.location);
+                    var search_params = url.searchParams;
+                    search_params.set('country', element);
+                    var new_url = url.toString();
+                    location.href = new_url;
+                }));     
+            });
+        }
+    
     }
+
+    // Should return to last page with history
+    function returnMain(){
+        location.href = "index.html";
+    }
+
+    const init = function (){
+        if (localStorage.getItem('mode') == 'light'){
+            toggleLightDarkMode();
+        }
+        displayCountry();
+        lightModeButton.addEventListener('click', toggleLightDarkMode)
+        backButton.addEventListener('click', (evt) => history.back())
+        homeButton.addEventListener('click', returnMain)
+    }
+
+    init();
+
+
 }
 
-// var startingCountry = sessionStorage.getItem("countryName");
-
-// Add api data to html file
-
-async function displayCountry() {
-    let params = new URLSearchParams(window.location.search)
-    let countryUrl = params.get('country')
-
-    const res = await fetch("https://restcountries.com/v3.1/alpha/" + countryUrl)
-  
-    let countryData = await res.json();
-  
-    countryData = countryData[0];
-
-
-    // console.log(countryUrl);
-    
-    document.querySelector('#country-flag').src = countryData.flags.svg
-    
-    document.querySelector("#official-name").textContent = countryData.name.common;
-    
-    document.querySelector("#native-name").textContent = Object.values(countryData.name.nativeName)[0].common;
-    
-    document.querySelector("#population").textContent = countryData.population.toLocaleString('en-UK').replaceAll(',','.');
-    
-    document.querySelector("#region").textContent = countryData.region;
-
-    document.querySelector("#sub-region").textContent = countryData.subregion;
-    
-    document.querySelector("#capital").textContent = countryData.capital;
-    
-    document.querySelector("#top-level-domain").textContent = countryData.tld;
-    
-    // Currencies
-    let currencies = [];
-    for(var el in countryData.currencies){
-        currencies.push(countryData.currencies[el].name);
-    }
-    let currenciesString = currencies.join(', ');
-    document.querySelector("#currencies").textContent = currenciesString;
-
-    // Languages
-    let languages = [];
-    for(var el in countryData.languages){
-        languages.push(countryData.languages[el]);
-    }
-    let languagesString = languages.join(', ');
-    document.querySelector("#languages").textContent = languagesString;
-
-    // Borders
-    bordersContainer = document.querySelector('#borders-container');
-    if (countryData.borders !== undefined){
-        countryData.borders.forEach(element => {
-
-            let countryListItem = document.createElement('li');
-            countryListItem.classList.add('details-button');
-            bordersContainer.appendChild(countryListItem);
-    
-            fetch("https://restcountries.com/v3.1/alpha/" + element)
-            .then((response) => response.json())
-            .then((data) => countryListItem.textContent = data[0].name.common)
-            .then((data) => countryListItem.addEventListener('click', function(){
-                sessionStorage.setItem('countryName' ,data.cca2);
-                var url = new URL(window.location);
-                var search_params = url.searchParams;
-                search_params.set('country', element);
-                var new_url = url.toString();
-                location.href = new_url;
-            }));     
-        });
-    }
-
-}
-
-if (sessionStorage.getItem('mode') == 'light'){
-    toggleLightDarkMode();
-}
-
-displayCountry();
-
-// Should return to last page with history
-function returnMain(){
-    var para = new URLSearchParams();
-    if (document.querySelector('body').getAttribute('data-theme') == 'light'){
-        para.append("light-mode", 'true');
-    }
-    location.href = "index.html?" + para.toString();
-}
-
+let ccapi = countryDetailsApi();
